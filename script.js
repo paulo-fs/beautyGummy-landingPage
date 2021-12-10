d017d2b1f13c66ed661394e631c143f81720922f
paulo-fs <jr.paulo87@gmail.com>
2021-12-10 12:00:20 -0300

    corrigindo bug no menu

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const menuMobile = document.querySelector('.menu-mobile');
 const closeBtn = document.querySelector('.menu-mobile-close');
 const openBtn = document.querySelector('.menu-mobile-icon');
+const mobileMenuItem = document.querySelectorAll('.close-menu');
+
 
 openBtn.addEventListener('click', () => {
     menuMobile.classList.remove('inactive');
@@ -9,3 +11,9 @@ openBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     menuMobile.classList.add('inactive');
 })
+
+for (item of mobileMenuItem){
+    item.addEventListener('click', () => {
+        menuMobile.classList.add('inactive');
+    })
+}
