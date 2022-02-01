30d4a1d1a6f40f6ff250f86e3d8bb360edf2d1f9
Paulo Souza <jr.paulo87@gmail.com>
2022-02-01 12:03:44 -0300

    ajustes e refatoração das sessões em arquivos separados

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const menuMobile = document.querySelector('.menu-mobile');
 const closeBtn = document.querySelector('.menu-mobile-close');
 const openBtn = document.querySelector('.menu-mobile-icon');
 const mobileMenuItem = document.querySelectorAll('.close-menu');
+const btnComprar = document.querySelector('#btnComprar');
 
 
 openBtn.addEventListener('click', () => {
@@ -17,3 +18,11 @@ for (item of mobileMenuItem){
         menuMobile.classList.add('inactive');
     })
 }
+
+btnComprar.addEventListener('click', () => {
+    const url = 'https://farmaciaqualifarma.com.br/harmonize-beauty-gummy-30-gomas-com-verisol-2-5g-silicio-vitaminas-e-minerais.html';
+
+    window.open(url, '_blank');
+    console.log('comprar');
+})
+
